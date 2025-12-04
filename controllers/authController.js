0a3c07c3106567a71444bed2e6767545177e52d0
aryan-v12 <aryanverma86301@gmail.com>
2025-12-04 05:41:08 +0530

    Replace bcrypt with bcryptjs for deployment

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 require('dotenv').config();
 
 const JWT_SECRET = process.env.JWT_SECRET;
